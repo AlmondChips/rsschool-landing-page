2fc47033b59ba5ea1ea901c469006d5a0d69f333
almondchips <chips6t@gmail.com>
2026-09-22 14:06:59 +0300

    fix: semantics

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 import "./styles/main.scss";
 import "./modules/theme";
+
+const menuBtn = document.querySelector("button.greeting-menu");
+
+menuBtn.addEventListener("click", () => {
+  window.location.href = "./src/pages/menu.html";
+});
